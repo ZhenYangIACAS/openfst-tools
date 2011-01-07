a9a0534b4944c864dfb33bb7820d28b918e74d8c
christopher.paul.taylor@gmail.com <christopher.paul.taylor@gmail.com@110e7e20-1fcf-d6c3-3edc-8e7a94a28820>
2011-01-07 21:06:21 +0000

    added initial class structure for fstprintpaths split out fstprintpaths main function into a separate file added a utilities file for miscellaneous, but reusable, function calls

---
+++ trunk/utils.hpp
@@ -0,0 +1,16 @@
+#ifndef __OPENFSTTOOLSUTILS__
+#define __OPENFSTTOOLSUTILS__
+
+#include "utils.hpp"    
+
+#include <vector>
+#include <string>
+
+using namespace std;
+
+string vectorToString(vector<int>& v);
+
+// convert int to string from Bjarne Stroustrup's FAQ
+string itos(int i);
+
+#endif
